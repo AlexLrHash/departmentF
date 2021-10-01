2f07023be71971a1737ae5f9658a921fb7da28fb
AlexeyRyzhakov <ryzhakovalexeynicol@gmail.com>
2021-10-01 15:55:40 +0300

    department filter

--- components/AdminDepartments.vue
+++ components/AdminDepartments.vue
@@ -1,6 +1,21 @@
 <template>
   <div style="margin-top: 100px;">
     <div class="table-responsive">
+      <div class="container row">
+        <div class="ml-2">
+          <input type="text" v-model="departmentName" placeholder="Укажите название" class="form-control">
+        </div>
+        <div class="ml-2">
+          <input type="text" v-model="departmentManager" placeholder="Укажите заведующего" class="form-control">
+          <div class="" style="position: absolute; background: white; opacity: .9; color: white">
+            <div v-for="manager in departmentManagers" style="cursor: pointer" @click="selectManager(manager.name)">{{ manager.name }}</div>
+          </div>
+        </div>
+        <div class="ml-2">
+          <button class="btn btn-success" @click="getDepartments">Поиск</button>
+        </div>
+      </div>
+      <br>
       <table class="table table-striped table-sm">
         <thead>
         <tr>
@@ -26,7 +41,15 @@ export default {
   name: "AdminDepartments",
   data() {
     return {
-      departments: ""
+      departments: "",
+      departmentName: "",
+      departmentManagers: "",
+      departmentManager: ""
+    }
+  },
+  watch: {
+    departmentManager: async function (val) {
+      await this.getManagers(val);
     }
   },
   async mounted() {
@@ -34,7 +57,7 @@ export default {
   },
   methods: {
     async getDepartments() {
-      const response = await fetch('http://localhost:8000/api/admin/departments', {
+      const response = await fetch(`http://localhost:8000/api/admin/departments?name=${this.departmentName}&manager=${this.departmentManager}`, {
         headers: {
           'Content-Type': 'application/json',
           "Accept": "application/json",
@@ -45,6 +68,23 @@ export default {
       } else {
         this.departments = await response.json();
         this.departments = this.departments.data;
+      }
+    },
+    selectManager(managerName) {
+      this.departmentManager = managerName;
+    },
+    async getManagers(val) {
+      const response = await fetch(`http://localhost:8000/api/admin/users?name=${val}&email=&role=MANAGER`, {
+        headers: {
+          'Content-Type': 'application/json',
+          "Accept": "application/json",
+          'Authorization': "Bearer " + localStorage.getItem('jwt')
+        },
+      });
+      if (response.status == 500) {
+      } else {
+        this.departmentManagers = await response.json();
+        this.departmentManagers = this.departmentManagers.data;
       }
     }
   }
